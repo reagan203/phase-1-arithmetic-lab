573e5497b6bb6a22d03e5bd3d24dee8ecd82c01a
reagan <reaganm746@gmail.com>
2023-09-21 20:51:19 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 =2;
+const num2 =31;
+const multiply =num1 * num2;
+//console.log (multiply)
+
+
+const random = Math.floor(Math.random()*10)
+
+const num3 =16;
+const num4 =6;
+const mod = 16%6;
+
+const max = Math .max (12,3,7,5,16,20)
+
+
